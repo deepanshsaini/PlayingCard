0eb8ff9361a8ac1bc63701095fe5dfb1ebb201d8
Deepansh Saini <sainideepansh5@gmail.com>
2019-09-20 23:25:21 +0530

    CustomStringConvertiable Class Inherited

--- PlayingCard/PlayingCard.swift
+++ PlayingCard/PlayingCard.swift
@@ -8,21 +8,26 @@
 
 import Foundation
 
-struct PlayingCard {
+struct PlayingCard : CustomStringConvertible
+{
+    
+    var description: String { return "\(rank)\(suit)" }
     
     var suit : Suit
     var rank : Rank
     
-    enum Suit: String {
+    enum Suit: String, CustomStringConvertible {
         case spades = "♠️"
         case hearts = "❤️"
         case diamonds = "♦️"
         case clubs = "♣️"
         
         static var all = [Suit.spades,.hearts,.diamonds,.clubs]
+        
+        var description : String { return rawValue}
     }
     
-    enum Rank {
+    enum Rank: CustomStringConvertible  {        
         case ace
         case face(String)
         case numeric(Int)
@@ -46,7 +51,13 @@ struct PlayingCard {
             allRanks += [Rank.face("J"),.face("Q"),.face("K")]
             return allRanks
         }
+        
+        var description: String {
+            switch self {
+            case .ace: return "A"
+            case .numeric(let pips): return String(pips)
+            case .face(let kind): return kind
+            }
+        }
     }
-    
-    
 }
